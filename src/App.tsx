d81a673db99b464834f1e3e4bf8edbf06d9f3bcc
dimka <dimka8445484@gmail.com>
2023-06-20 21:59:32 +0200

    added: useTheme and styles config

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React, { Suspense } from "react";
-import "./index.scss";
-
-import { AboutPageLazy, MainPageLazy } from "./pages";
 import { Link, Route, Routes } from "react-router-dom";
 
+import { AboutPageLazy, MainPageLazy } from "./pages";
+import { useTheme } from "./hooks";
+
+import "./styles/index.scss";
+
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading...</div>}>
