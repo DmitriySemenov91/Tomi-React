3fb3d189bd788bda60957d58740854c68ff35d8c
dimka <dimka8445484@gmail.com>
2023-06-21 15:10:50 +0200

    added: custom classNames func

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,16 @@ import { Link, Route, Routes } from "react-router-dom";
 
 import { AboutPageLazy, MainPageLazy } from "./pages";
 import { useTheme } from "./hooks";
+import { classNames } from "./helpers";
 
 import "./styles/index.scss";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
+  //className={classNames("app", { hovered: true, selected: false }, [theme])}
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle Theme</button>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
